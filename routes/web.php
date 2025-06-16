598bfc64b631df92cd02ea8c55fcec22aad5bed0
V1OLE123 <longnguyenevil@gmail.com>
2025-06-16 17:53:35 +0700

    #10 Hoàn thành xem, thêm, sửa môn học

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MonHocController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/monhoc', [MonHocController::class, 'index'])->name('monhoc.index');
+Route::get('/monhoc/{id}/edit', [MonHocController::class, 'edit'])->name('monhoc.edit');
+Route::put('/monhoc/{id}', [MonHocController::class, 'updateMonHoc'])->name('monhoc.update');
+Route::get('/monhoc/create', [MonHocController::class, 'create'])->name('monhoc.create');
+Route::post('/monhoc', [MonHocController::class, 'addMonhoc'])->name('monhoc.store');
