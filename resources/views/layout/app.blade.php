063f718d8096a993dcbf596f936c8592f4e6bdaa
Khoa Nguyễn <0306221240@caothang.edu.vn>
2025-07-01 16:41:24 +0700

    Khoa sửa lại model DeThi và giao diện profile của quản trị và giảng viên

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,6 +7,8 @@
     <title>Quản lý hệ thống thi trắc nghiệm trực tuyến</title>
     <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/favicon.png') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/styles.min.css') }}" />
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     @yield('CssProfile')
 </head>
